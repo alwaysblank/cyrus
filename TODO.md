c8cd287b7a0da06d826fbbdf7c353df95c19335e
Ben Bateman <ben@murmurcreative.com>
2016-05-25 15:52:30 -0700

    Updated tests and todo.md

---
+++ TODO.md
@@ -0,0 +1,5 @@
+## Features
+
+- Auto-detect certain things, i.e.:
+    - When `setURL` is used, automatically set element to `a`
+    
--- tests/CyrusTest.php
+++ tests/CyrusTest.php
@@ -9,5 +9,11 @@ class CyrusTest extends PHPUnit_Framework_TestCase {
     $element = new Cyrus;
     $this->assertContains( 'test-class', $element->setClass( 'test-class' )->addContent( 'This is a test' )->construct() );
   }
+
+  public function testCyrusUnterminatedChild()
+  {
+    $test = new Livy\Cyrus;
+    $this->assertContains( 'test-outer', $test->setClass('test-outer')->openChild()->setClass('inner')->addContent('test content')->construct() );
+  }
  
 }
